2f505c702c7e6ecd7a8ecbba85b310a1961933d4
leonel indurain <leonelindurain95@gmail.com>
2022-10-20 21:08:46 -0300

    TP13

---
+++ TP13/src/daos/index.js
@@ -0,0 +1,11 @@
+require("dotenv").config()
+
+const CarritoDaoMongoDB = require("./carritos/CarritoDaoMongoDB.js")
+const ProductoDaoMongoDB = require("./productos/ProductoDaoMongoDB.js")
+const LoginDaoMongoDB = require("./login/LoginDaoMongoDB.js")
+const ChatDaoFirebase = require("./chat/ChatDaoFirebase.js")
+
+exports.Carrito = CarritoDaoMongoDB
+exports.Producto = ProductoDaoMongoDB
+exports.Login = LoginDaoMongoDB
+exports.Chat = ChatDaoFirebase
