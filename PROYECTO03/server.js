81336e528073b8b8115a1687dafef766cc1b6b0e
leonel indurain <leonelindurain95@gmail.com>
2022-11-10 16:48:15 -0300

    2do commit

--- PROYECTO03/server.js
+++ PROYECTO03/server.js
@@ -9,6 +9,16 @@ require("dotenv").config()
 
 const app = express()
 
+// MIDDLEWARE //
+
+app.use(cp())
+const checkAuthentication = require("./microService/config/checkAuthentication")
+const passport = require("./microService/config/passportMiddleware")
+
+app.use(express.urlencoded({ extends: true}))
+app.use(express.json())
+app.use(express.static('public'))
+
 const PORT = process.env.PORT || 8080
 
 app.set("view engine", "hbs")
@@ -31,7 +41,7 @@ app.use(
 				useUnifiedTopology: true
 			}
 		}),
-		secret: process.env.MONGODB_SECRETO || "secreto",
+		secret: process.env.MONGODB_SECRETO,
 		resave: false,
 		rolling: true,
 		saveUninitialized: false,
@@ -43,30 +53,32 @@ app.use(
 	})
 )
 
-// MIDDLEWARE //
-
-app.use(cp())
-const passport = require("./microService/config/passportMiddleware")
-app.use(express.urlencoded({ extends: true}))
-app.use(express.json())
-app.use(express.static('public'))
-
 // PASSPORT //
 
 app.use(passport.initialize())
 app.use(passport.session())
 
+// INICIO //
+
+const ContenedorProductos = require('./microService/Productos/Daos/ProductosDaoMongoDB')
+const contenedorProductos = new ContenedorProductos
+
+app.get("/", checkAuthentication, async (req, res) => {
+	const productos = await contenedorProductos.getAll();
+	res.render("index", { productos });
+})
+
 // REGISTER //
 
 app.get("/register", (req, res) => {
-	res.render("register.hbs")
+	res.render("register")
 })
 
 app.post(
 	"/register",
-	passport.authenticate("register.hbs", {
-		failureRedirect: "failregister.hbs",
-		successRedirect: "login.hbs"
+	passport.authenticate("register", {
+		failureRedirect: "failregister",
+		successRedirect: "login"
 	}),
 	(req, res) => {
 		res.render("/login", { username: req.body.username })
@@ -74,8 +86,44 @@ app.post(
 )
 
 app.get("/failregister", (req, res) => {
-	console.error("Error de registro")
-	res.render("failregister.hbs")
+	console.error("Error de Registro")
+	res.render("failregister")
+})
+
+// LOGIN //
+
+app.get("/login", (req, res) => {
+	if (req.isAuthenticated()) {
+		let user = req.user
+		console.log("Usuario Logueado")
+		res.render("index")
+	} else {
+		console.log("Usuario no logueado")
+		res.render("login")
+	}
+})
+
+app.post(
+	"/login",
+	passport.authenticate("login", {
+		successRedirect: "/",
+		failureRedirect: "faillogin"
+	}),
+
+	(req, res) => {
+		res.render("/", { username: req.body.username })
+	}
+)
+
+// LOGOUT //
+
+app.get("/logout", async (req, res = response, next) => {
+	req.logout(err => {
+		if (err) {
+			return next(err)
+		}
+		res.redirect("/")
+	})
 })
 
 // CARRITO //
@@ -92,7 +140,6 @@ routerCarrito.delete('/:idCart/productos/:idProduct', deleteProductCart)
 // PRODUCTOS //
 
 const { getProduct, getProductId, postProduct, putProduct, deleteProduct } = require("./microService/Productos/controllers/ProductosController")
-const checkAuthentication = require("./microService/config/checkAuthentication")
 const routerProductos = Router()
 
 routerProductos.get('/', getProduct)
@@ -103,6 +150,15 @@ routerProductos.delete('/:id', checkAuthentication, deleteProduct)
 
 app.use('/api/productos', routerProductos)
 app.use('/api/carrito', routerCarrito)
+
+// LOGGER //
+
+app.use((req, res, next) => {
+	logger.warn("URL NOT FOUND")
+	res.sendStatus("404")
+})
+
+// LISTENER //
 
 app.listen(PORT, err => {
     if(err) throw err
